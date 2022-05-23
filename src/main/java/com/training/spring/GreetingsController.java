f331c12e4f13d668f0846ab25196b40c25bd6734
osman <osman@osman123.com>
2022-05-23 16:47:35 +0300

    day 1 son

--- src/main/java/com/training/spring/GreetingsController.java
+++ src/main/java/com/training/spring/GreetingsController.java
@@ -1,6 +1,9 @@
 package com.training.spring;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
